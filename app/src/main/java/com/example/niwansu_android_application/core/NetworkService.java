9c24ccca91c0225b50cb845d2fd00ab658eeceb1
Kavindu Pathirana <76916101+KavinduPathirana98@users.noreply.github.com>
2023-05-02 00:56:51 +0530

    Added Profile Update

--- app/src/main/java/com/example/niwansu_android_application/core/NetworkService.java
+++ app/src/main/java/com/example/niwansu_android_application/core/NetworkService.java
@@ -83,69 +83,12 @@ public interface NetworkService {
     @POST("updateStatus.php")
     Call<changeStatusModel> changeStatus(@Field("req_id") int req_id, @Field("status") String status,@Field("time") String time);
 
-//    @FormUrlEncoded
-//    @POST("profile.php")
-//    Call<SetupProfileResponseModel> profile(@FieldMap HashMap<String, String> params);
-//
-//    @FormUrlEncoded
-//    @POST("TestHistory.php")
-//    Call<QuestionnaireResponseModel> upload(@FieldMap HashMap<String, String> params);
-//
-//
-//    @FormUrlEncoded
-//    @POST("DoctorSchedule.php")
-//    Call<DocResponseModel> doctorschedule(@FieldMap HashMap<String, String> params);
-//
-//
-//
-//
-//    @GET("getdoctorscheduledates.php")
-//    Call<List<Users>> getUsers(
-//
-//            @Query("key") String key,
-//            @Query("doctorid") String doctorid
-//
-//    );
-//    @GET("getdoctorscheduleddatesevening.php")
-//    Call<List<Users>> getUsers2(
-//
-//            @Query("key") String key,
-//            @Query("doctorid") String doctorid
-//
-//    );
-//
-//    @GET("FetchNotification.php")
-//    Call<List<Users>> getNotification(
-//
-//            @Query("patientid") String patientid
-//
-//    );
-//    @GET("getTestResults.php")
-//    Call<List<TestResults>> getTestResults(
-//
-//            @Query("userid") String patientid
-//
-//    );
-////    @FormUrlEncoded
-////    @POST("appointmentaccept.php")
-////    Call<AppointmentResponseModel> doctorappointmentaccept(@FieldMap HashMap<String, String> params);
-//
-//
-//    @FormUrlEncoded
-//    @POST("appointmentaccept.php")
-//    Call<AppointmentResponseModel> doctorappointmentaccept(
-//            @Field("key") String key,
-//            @Field("id") int id,
-//            @Field("doctorapprovalstatus") String appAccept,
-//              @Field("timeslotassigned") String timeslot);
-//
-//    @FormUrlEncoded
-//    @POST("appointmentaccept.php")
-//    Call<AppointmentResponseModel> doctorappointmentdeclined(
-//            @Field("key") String key,
-//            @Field("id") int id,
-//            @Field("doctorapprovalstatus") String appAccept);
-//
+
+    @FormUrlEncoded
+    @POST("updateProfile.php")
+    Call<changeStatusModel> updateProfile(@Field("fname") String fname, @Field("lname") String lname,@Field("email") String email,@Field("contact") String contact,@Field("password") String password);
+
+
 
 
 }
--- app/src/main/java/com/example/niwansu_android_application/screens/patient/activities/UpdateProfileActivity.java
+++ app/src/main/java/com/example/niwansu_android_application/screens/patient/activities/UpdateProfileActivity.java
@@ -4,17 +4,32 @@ import static com.example.niwansu_android_application.core.Constants.PREFERENCE_
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.niwansu_android_application.R;
+import com.example.niwansu_android_application.core.AppointmentsList;
 import com.example.niwansu_android_application.core.Constants;
+import com.example.niwansu_android_application.core.NetworkClient;
+import com.example.niwansu_android_application.core.NetworkService;
+import com.example.niwansu_android_application.models.changeStatusModel;
+import com.example.niwansu_android_application.screens.doctor.activities.ProfileActivity;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class UpdateProfileActivity extends AppCompatActivity {
 EditText txt_fname,txt_lname,txt_email,txt_contact,txt_password;
-TextView txt_tname;
+TextView txt_tname,txt_save;
+    private NetworkService apiInterface;
     SharedPreferences sharedPreferences;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +38,31 @@ TextView txt_tname;
         sharedPreferences =getSharedPreferences(PREFERENCE_NAME, MODE_PRIVATE);
         OnCreateObjects();
         setData();
+        txt_email.setEnabled(false);
+        apiInterface = NetworkClient.getClient().create(NetworkService.class);
+        txt_save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Call<changeStatusModel> call = apiInterface.updateProfile(txt_fname.getText().toString(),txt_lname.getText().toString(),txt_email.getText().toString(),txt_contact.getText().toString(),txt_password.getText().toString());
+
+                call.enqueue(new Callback<changeStatusModel>() {
+                    @Override
+                    public void onResponse(Call<changeStatusModel> call, Response<changeStatusModel> response) {
+                        if(response.body().getResult()==1)
+                        {
+                            Toast.makeText(getApplicationContext(),"Successfully Updated !",Toast.LENGTH_LONG).show();
+                            Intent intent=new Intent(getApplicationContext(), LoginActivity.class);
+                            startActivity(intent);
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<changeStatusModel> call, Throwable t) {
+                        Toast.makeText(getApplicationContext(),"Error Updated !",Toast.LENGTH_LONG).show();
+                    }
+                });
+            }
+        });
 
     }
     protected  void OnCreateObjects()
@@ -33,6 +73,7 @@ TextView txt_tname;
         txt_email=findViewById(R.id.txt_email);
         txt_password=findViewById(R.id.txt_password);
         txt_tname=findViewById(R.id.txt_tname);
+        txt_save=findViewById(R.id.txt_save);
     }
     protected  void setData()
     {
